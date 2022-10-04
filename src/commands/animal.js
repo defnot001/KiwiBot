e0eb4f9bac6bb3e11eef670a8efea15413cbbef0
defnot001 <marc.miller994@gmail.com>
2022-10-04 22:00:30 +0200

    removed redundant error catching

--- src/commands/animal.js
+++ src/commands/animal.js
@@ -18,36 +18,25 @@ module.exports = {
     ),
   async execute(interaction) {
     const animal = interaction.options.getString('animal');
-    try {
-      if (animal === 'fox') {
-        const data = await fetchAnimal('https://randomfox.ca/floof/');
-        interaction.reply(data.image);
-        return;
-      }
+    if (animal === 'fox') {
+      const data = await fetchAnimal('https://randomfox.ca/floof/');
+      interaction.reply(data.image);
+      return;
+    }
 
-      if (animal === 'cat') {
-        const data = await fetchAnimal(
-          'https://api.thecatapi.com/v1/images/search'
-        );
-        interaction.reply(data[0].url);
-        return;
-      }
+    if (animal === 'cat') {
+      const data = await fetchAnimal(
+        'https://api.thecatapi.com/v1/images/search'
+      );
+      interaction.reply(data[0].url);
+      return;
+    }
 
-      if (animal === 'dog') {
-        const data = await fetchAnimal(
-          'https://api.thedogapi.com/v1/images/search'
-        );
-        interaction.reply(data[0].url);
-        return;
-      }
-
-      interaction.reply('Something went wrong.');
-    } catch (err) {
-      console.error(err);
-      interaction.reply({
-        content: 'There was an error while executing this command!',
-        ephemeral: true,
-      });
+    if (animal === 'dog') {
+      const data = await fetchAnimal(
+        'https://api.thedogapi.com/v1/images/search'
+      );
+      interaction.reply(data[0].url);
     }
   },
 };
