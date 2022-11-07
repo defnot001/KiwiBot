db2a6552e37e36db03f41a33682c9435750370b0
defnot001 <marc.miller994@gmail.com>
2022-11-07 21:56:20 +0100

    started using dotenv

---
+++ src/config/guildConfig.template.js
@@ -0,0 +1,20 @@
+// you can change your favorite embed color here using the format 0x+6 hex characters
+
+const guildconfig = {
+  embedColor: '0x35aa78',
+  inviteChannelId: '',
+  memberRoleId: '',
+  adminRoleId: '',
+  resourcesChannelId: '',
+  serverInfoChannelId: '',
+  todoChannelId: '',
+  memberLogChannelId: '',
+  modLogChannelId: '',
+  emojis: {
+    kiwi: '',
+    owoKiwi: '',
+    froghypers: '',
+  },
+};
+
+export default guildconfig;
