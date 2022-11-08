9d702ce0c9c9456b8d823bed6d8701e64f92e78f
defnot001 <marc.miller994@gmail.com>
2022-11-08 21:03:07 +0100

    removed guildConfig from git and added template

---
+++ src/config/guildConfig.template.ts
@@ -0,0 +1,28 @@
+const guildConfig = {
+  channels: {
+    invite: '',
+    resources: '',
+    serverInfo: '',
+    todo: '',
+    memberLog: '',
+    modLog: '',
+  },
+  roles: {
+    member: '',
+    admin: '',
+  },
+  emojis: {
+    kiwi: '',
+    owoKiwi: '',
+    froghypers: '',
+  },
+  embedColor: {
+    defaut: 3_517_048,
+    none: 3_092_790,
+    green: 5_763_719,
+    yellow: 16_776_960,
+    error: 15_548_997,
+  },
+};
+
+export default guildConfig;
