e49d632952e67609a6c490d7d5146bcc2429495f
defnot001 <marc.miller994@gmail.com>
2022-11-10 09:49:47 +0100

    changed to match new file names

--- src/config/botConfig.ts
+++ src/config/botConfig.ts
@@ -1,22 +1,22 @@
 import dotenv from 'dotenv';
-import type IENV from '@interfaces/ENV';
-import type IBotConfig from '@interfaces/BotConfig';
+import type ENVInterface from '../util/interfaces/ENVinterface';
+import type BotConfigInterface from '../util/interfaces/BotConfigInterface';
 
 dotenv.config();
 
-const env: IENV = {
+const env: ENVInterface = {
   token: process.env.BOT_TOKEN,
   clientID: process.env.CLIENT_ID,
   guildID: process.env.GUILD_ID,
 };
 
-const checkConfig = (config: IENV) => {
+const checkConfig = (config: ENVInterface) => {
   for (const [key, value] of Object.values(env)) {
     if (!value) {
       throw new Error(`Missing key ${key} in .env`);
     }
   }
-  return config as IBotConfig;
+  return config as BotConfigInterface;
 };
 
 const botConfig = checkConfig(env);
