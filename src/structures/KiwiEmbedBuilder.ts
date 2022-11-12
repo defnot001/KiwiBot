c18b3f337bee26e3c1186c55f81b76ff5b91f350
defnot001 <marc.miller994@gmail.com>
2022-11-12 18:05:24 +0100

    extended EmbedBuilder instead of functional

---
+++ src/structures/KiwiEmbedBuilder.ts
@@ -0,0 +1,18 @@
+import { APIEmbed, EmbedBuilder, EmbedData } from 'discord.js';
+import type { User } from 'discord.js';
+import EmbedColor from '../config/embedColors';
+
+export class KiwiEmbedBuilder extends EmbedBuilder {
+  public constructor(user: User, data?: EmbedData | APIEmbed) {
+    super(data);
+
+    this.setColor(EmbedColor.default);
+
+    this.setFooter({
+      text: `Requested by ${user.username}`,
+      iconURL: user.displayAvatarURL(),
+    });
+
+    this.setTimestamp(Date.now());
+  }
+}
