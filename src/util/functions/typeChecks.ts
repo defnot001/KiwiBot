457d7cc3ec80a28f48e9548eee6216c7f137e097
defnot001 <marc.miller994@gmail.com>
2022-11-18 17:34:43 +0100

    added more functions to check types

--- src/util/functions/typeChecks.ts
+++ src/util/functions/typeChecks.ts
@@ -1,5 +1,18 @@
-import { GuildMember } from 'discord.js';
+import { GuildBasedChannel, GuildMember, TextChannel } from 'discord.js';
+import type { PartialGuildMember } from 'discord.js';
 
 export function isGuildMember(value: unknown): value is GuildMember {
   return value instanceof GuildMember;
+}
+
+export function isTextChannel(
+  channel: GuildBasedChannel,
+): channel is TextChannel {
+  return channel instanceof TextChannel;
+}
+
+export async function getMemberFromPartial(
+  maybeMember: GuildMember | PartialGuildMember,
+): Promise<GuildMember> {
+  return maybeMember.partial ? await maybeMember.fetch() : maybeMember;
 }
