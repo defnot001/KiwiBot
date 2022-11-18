f13a2684378642bcc200b9721041ad2e13360b99
defnot001 <marc.miller994@gmail.com>
2022-11-18 22:25:16 +0100

    made event work as intended

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,6 +1,6 @@
 import { userMention, inlineCode, time } from 'discord.js';
 import { Event } from '../structures/Event';
-import { getLogChannels } from '../util/functions/loggers';
+import { errorLog, getLogChannels } from '../util/functions/loggers';
 import { JoinLeaveEmbedBuilder } from '../structures/Embeds/JoinLeaveEmbedBuilder';
 import {
   colorFromDuration,
@@ -8,27 +8,37 @@ import {
 } from '../util/functions/helpers';
 
 export default new Event('guildMemberAdd', async (member) => {
-  console.log(`${member.user.tag} joined ${member.guild.name}.`);
+  try {
+    console.log(`${member.user.tag} joined ${member.guild.name}.`);
 
-  const { memberLog } = getLogChannels(member.guild);
+    const { memberLog } = getLogChannels(member.guild);
 
-  const joinedAt = getJoinedAtComponent(member);
+    const joinedAt = getJoinedAtComponent(member);
 
-  const accountAge: number =
-    new Date().valueOf() - member.user.createdAt.valueOf();
+    const accountAge: number =
+      new Date().valueOf() - member.user.createdAt.valueOf();
 
-  const embedColor: number = colorFromDuration(accountAge) || 3_092_790;
+    const embedColor: number = colorFromDuration(accountAge) || 3_092_790;
 
-  const joinEmbed = new JoinLeaveEmbedBuilder(member, 'joined', {
-    color: embedColor,
-    description: `Username: ${userMention(member.user.id)}\n
-    User ID: ${inlineCode(member.user.id)}${joinedAt}
-    \n
-    Created at: ${time(member.user.createdAt, 'f')} (${time(
-      member.user.createdAt,
-      'R',
-    )})`,
-  });
+    const joinEmbed = new JoinLeaveEmbedBuilder(member, 'joined', {
+      description: `Username: ${userMention(
+        member.user.id,
+      )}\nUser ID: ${inlineCode(member.user.id)}${joinedAt}\nCreated at: ${time(
+        member.user.createdAt,
+        'f',
+      )} (${time(member.user.createdAt, 'R')})`,
+    });
 
-  memberLog.send({ embeds: [joinEmbed] });
+    joinEmbed.setColor(embedColor);
+
+    memberLog.send({ embeds: [joinEmbed] });
+  } catch (err) {
+    console.error(err);
+    errorLog({
+      client: member.client,
+      guild: member.guild,
+      type: 'error',
+      errorMessage: `'GuildMemberAdd' Event triggered but the embed could not be sent.`,
+    });
+  }
 });
